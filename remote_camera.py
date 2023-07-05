644bfe8624f2c233d6ac5d89cc623879a1bbd3d2
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-05 18:15:08 -0400

    Robust control flow, and network session reuse.

--- remote_camera.py
+++ remote_camera.py
@@ -14,6 +14,8 @@ class VideoStream:
     button_stop = None
     status = "Idle..."
 
+    run = False
+
     thread = None
 
     host = "localhost"
@@ -25,60 +27,95 @@ class VideoStream:
 
     def __init__(self):
     
+        # Check camera state
         self.video_cap = cv2.VideoCapture(0)
-        self.run = False
+        VideoStream.run = True if self.video_cap.isOpened() else False
 
-        if self.video_cap.isOpened():
-            self.return_val, _ = self.video_cap.read()
-            self.run = True
+        if VideoStream.run is False:
+            VideoStream.update_status("Camera not found ...")
+            VideoStream.press_stop()
+
         else:
-            self.return_val = False
+            try:
+                VideoStream.write_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                VideoStream.write_socket.connect((VideoStream.host, VideoStream.port))
+            except ConnectionRefusedError as E:
+                VideoStream.update_status("Connection refused ...")
+                VideoStream.press_stop()
 
-        VideoStream.write_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        VideoStream.write_socket.connect((VideoStream.host, VideoStream.port))
+    def delete(self):
+        self.video_cap.release()
+        if VideoStream.write_socket is not None:
+            VideoStream.write_socket.close()
+            VideoStream.write_socket = None
 
     @classmethod
     def press_stop(cls):
-        VideoStream.update_status("Stopped Transmitting ...")
-        VideoStream.obj.run = False
-        VideoStream.thread.join()
-        VideoStream.thread = None
+
         VideoStream.button_stop.pack_forget()
         VideoStream.button_stop = None
+        VideoStream.run = False
+        if VideoStream.status == "Transmitted ...":
+            VideoStream.thread.join()
+            # VideoStream.write_socket.sendall(b"")
+            VideoStream.update_status("Stopped Transmitting ...")
+            VideoStream.thread = None           
+        
+        if VideoStream.obj is not None:
+            VideoStream.obj.delete()
+            del VideoStream.obj
+            VideoStream.obj = None
 
     @classmethod
     def update_status(cls, status):
-        print(f"Status Update: {status}")
-        VideoStream.status = status
-        VideoStream.text.set(VideoStream.status)
+
+        if status != VideoStream.status:
+            print(f"Status Update: {status}")
+            VideoStream.status = status
+            VideoStream.text.set(VideoStream.status)
 
     @classmethod
     def press_start(cls):
-
+        
+        # Double clicking start 
         if VideoStream.thread is not None:
             return
 
-        if VideoStream.button_stop is None:
-            VideoStream.update_status("Starting up ...")
-            button_stop = tk.Button(VideoStream.root, text="STOP", command=VideoStream.press_stop, bg="red")
-            VideoStream.button_stop = button_stop
-            button_stop.config(width=35, height=10)
-            button_stop.place(x=300 , y=300)
+        VideoStream.update_status("Starting up ...")
 
-            button_stop.pack(ipadx=5, ipady=5, expand=5)
+        # Create stop button
+        button_stop = tk.Button(VideoStream.root, text="STOP", command=VideoStream.press_stop, bg="red")
+        VideoStream.button_stop = button_stop
+        button_stop.config(width=35, height=10)
+        button_stop.place(x=300 , y=300)
+        button_stop.pack(ipadx=5, ipady=5, expand=5)
 
-            VideoStream.obj = VideoStream()
+        # Initilize connection
+        VideoStream.obj = VideoStream()
+
+        if VideoStream.status != "Connection refused ...":
             VideoStream.thread = threading.Thread(target=VideoStream.image_capture_thread)
             VideoStream.update_status("Capture started ...")
             VideoStream.thread.start()
 
     @classmethod
     def image_capture_thread(cls):
-        while VideoStream.obj.run is True:
-            return_value , frame = VideoStream.obj.video_cap.read()
-            VideoStream.update_status("Frame captured ...")
-            VideoStream.transmit_frame(frame)
-            VideoStream.update_status("Transmitted ...")
+        while VideoStream.run is True:
+            try:
+                return_value , frame = VideoStream.obj.video_cap.read()
+            except:
+                VideoStream.update_status("Camera closed ...")
+                VideoStream.press_stop()
+                return
+            
+            try:
+                VideoStream.transmit_frame(frame)
+                VideoStream.update_status("Transmitted ...")
+            except:
+                VideoStream.update_status("Transmission failed ...")
+                VideoStream.press_stop()
+                return
+
 
         VideoStream.obj.video_cap.release()
 
--- server.py
+++ server.py
@@ -4,36 +4,63 @@ import struct
 import socket
 import cv2
 import matplotlib.pyplot as plt
-
+import time
 host = ""
 port = 2905
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(3)
 
 s.bind((host, port))
 s.listen(10)
 
-connection, address = s.accept()
-data = bytearray()
-message_size = struct.calcsize("L")
+def service_connection():
+    data = bytearray()
+    message_size = struct.calcsize("L")
+    count = 0
+    connection_valid = True
+    
+    while True:
+        while len(data) < message_size:
+            packet = connection.recv(4096)
+            if packet == b'': 
+                connection_valid = False
+                break
+            else: 
+                data += packet
+        
+        if connection_valid is False:
+            break
+        
+        data, packed_msg_size = data[message_size:], data[:message_size]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+        
+        while len(data) < msg_size:            
+            packet = connection.recv(4096)
+            if packet == b'': 
+                connection_valid = False
+                break
+            else: 
+                data += packet
+        
+        if connection_valid is False:
+            break
+
+        data, frame_data = data[msg_size:], data[:msg_size]
+        frame = pickle.loads(frame_data)
+        print(f"FRAME ({count})")    
+        count += 1
+        # plt.imshow(frame)
+        # plt.show()
 
 while True:
+    time.sleep(1)
+    print(f"Attempting connection ... ")
 
-    while len(data) < message_size:
-        data += connection.recv(4096)
-
-    packed_msg_size = data[:message_size]
-    data = data[message_size:]
-
-    msg_size = struct.unpack("L", packed_msg_size)[0]
+    try:
+        connection, address = s.accept()
+        service_connection()
     
-    while len(data) < msg_size:
-        data += connection.recv(4096)
-
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    
-    frame = pickle.loads(frame_data)
-    plt.imshow(frame)
-
+    except:
+        print(f"Connection Failed ...")
     
