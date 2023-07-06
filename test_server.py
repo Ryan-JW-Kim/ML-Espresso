c79f96a9ee9da10626a9b70097366257e16815d0
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-05 20:14:58 -0400

    Updates

--- test_server.py
+++ test_server.py
@@ -1,7 +1,8 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HOST = ""  # The server's hostname or IP address
+PORT = 0  # The port used by the server
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
