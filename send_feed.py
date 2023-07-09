f7f95024331885c5921d2b43483b73ff86894ef0
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-09 13:31:34 -0400

    fix?

--- send_feed.py
+++ send_feed.py
@@ -5,28 +5,26 @@ import time
 import pickle
 import struct
 
+host = "192.168.1.188"
+port = 8080
+
 while True:
-
-    host = "192.168.1.188"
-    port = 8080
-
     output_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     output_socket.connect((host, port))
     print(f"Connection Established ...")
 
-    try:
+    try:    
         cap = cv.VideoCapture(0)
-        
-        if cap.isOpened():
-            print("Success fully capturing frame ...")
-        while cap.isOpened():
 
+        if cap.isOpened(): print("Success fully capturing frame ...")
+        
+        while cap.isOpened():
             ret, frame = cap.read()
             data = pickle.dumps(frame)
             output_socket.sendall(struct.pack("L", len(data))+data)
-        
+
         cap.release()
-    
+
     except Exception as e:
 
         print(f"Error ... {e}")
