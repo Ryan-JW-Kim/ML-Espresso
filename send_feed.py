8845b1839a32cbf4abfb41ea267e41b2b163a5b1
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-09 13:28:24 -0400

    temporary camera scheme

---
+++ send_feed.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+import socket
+import time
+import pickle
+import struct
+
+while True:
+
+    host = "192.168.1.188"
+    port = 8080
+
+    output_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    output_socket.connect((host, port))
+    print(f"Connection Established ...")
+
+    try:
+        cap = cv.VideoCapture(0)
+        
+        if cap.isOpened():
+            print("Success fully capturing frame ...")
+        while cap.isOpened():
+
+            ret, frame = cap.read()
+            data = pickle.dumps(frame)
+            output_socket.sendall(struct.pack("L", len(data))+data)
+        
+        cap.release()
+    
+    except Exception as e:
+
+        print(f"Error ... {e}")
+        time.sleep(5)
