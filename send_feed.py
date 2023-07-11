b355c4b9c7b3efd4cca9339d7bb36a633e589029
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-10 23:56:28 -0400

    updates

--- send_feed.py
+++ send_feed.py
@@ -23,7 +23,12 @@ while True:
             data = pickle.dumps(frame)
             output_socket.sendall(struct.pack("L", len(data))+data)
 
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
         cap.release()
+        cv2.destroyAllWindows()
 
     except Exception as e:
         
