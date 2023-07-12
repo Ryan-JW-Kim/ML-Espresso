b04375bdb360645d2b977a800414893ef10ac644
Ryan Kim <rj.kim.toronto@gmail.com>
2023-07-11 22:03:53 -0400

    Change File Layout

--- Application/server.py
+++ Application/server.py
@@ -23,13 +23,13 @@ def fake_connection():
         for file in files:
             # time.sleep(.45)
             with open(f"images\\{file}", "rb") as input:
-                yield pickle.load(input)
+                yield (file, pickle.load(input))
 
 def save_fake_frame(frame, id):
     with open(f"images\\frame_{id}", "wb") as fh:
         pickle.dump(frame, fh, pickle.HIGHEST_PROTOCOL)
     
-def service_connection():
+def service_connection(connection):
     data = bytearray()
     message_size = struct.calcsize("L")
     count = 0
@@ -63,55 +63,54 @@ def service_connection():
 
         data, frame_data = data[msg_size:], data[:msg_size]
         frame = pickle.loads(frame_data)
-        time.sleep(1)
         yield frame
 
 def bound_frame(frame):
 
-    imgBlur = cv2.GaussianBlur(frame, (7,7), 2)
-    gray = cv2.cvtColor(imgBlur,  cv2.COLOR_BGR2GRAY)
+    # imgBlur = cv2.GaussianBlur(frame, (7,7), 2)
+    # gray = cv2.cvtColor(imgBlur,  cv2.COLOR_BGR2GRAY)
 
-    thresh1 =cv2.getTrackbarPos("Thresh1", "Parameters")
-    thresh2 =cv2.getTrackbarPos("Thresh2", "Parameters")
-    imgCanny = cv2.Canny(gray, thresh1, thresh2)
+    # thresh1 =cv2.getTrackbarPos("Thresh1", "Parameters")
+    # thresh2 =cv2.getTrackbarPos("Thresh2", "Parameters")
+    # imgCanny = cv2.Canny(gray, thresh1, thresh2)
 
-    kernel = np.ones((5,5))
-    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    # kernel = np.ones((5,5))
+    # imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
-    contours, hierarchy = cv2.findContours(imgDil, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # contours, hierarchy = cv2.findContours(imgDil, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     
-    if not Clusters.start_kmeans:
-        for contour in contours:
-            a = cv2.contourArea(contour)
-            min_  = cv2.getTrackbarPos("MinArea", "Parameters")
-            max_ = cv2.getTrackbarPos("MaxArea", "Parameters")
-            # min_ = 0
-            # max_ = 10000
-            if a > min_ and a < max_:
-                cv2.drawContours(frame, contour, -1 ,(0, 255, 255), 7)
+    # if not Clusters.start_kmeans:
+    #     for contour in contours:
+    #         a = cv2.contourArea(contour)
+    #         min_  = cv2.getTrackbarPos("MinArea", "Parameters")
+    #         max_ = cv2.getTrackbarPos("MaxArea", "Parameters")
+    #         # min_ = 0
+    #         # max_ = 10000
+    #         if a > min_ and a < max_:
+    #             cv2.drawContours(frame, contour, -1 ,(0, 255, 255), 7)
 
-                if Clusters.all_contours is None:
-                    Clusters.all_contours = np.array(contour)
+    #             if Clusters.all_contours is None:
+    #                 Clusters.all_contours = np.array(contour)
             
-                else:
-                    if len(Clusters.all_contours) >= Clusters.max_points:
-                        Clusters.start_kmeans = True
-                        print("START")
-                        break
-                    else:
-                        print(Clusters.all_contours.shape)
-                        print(contour.shape)
-                        Clusters.all_contours = np.append(Clusters.all_contours, contour, axis=0)
+    #             else:
+    #                 if len(Clusters.all_contours) >= Clusters.max_points:
+    #                     Clusters.start_kmeans = True
+    #                     print("START")
+    #                     break
+    #                 else:
+    #                     print(Clusters.all_contours.shape)
+    #                     print(contour.shape)
+    #                     Clusters.all_contours = np.append(Clusters.all_contours, contour, axis=0)
 
 
-    if Clusters.start_kmeans:
-        Clusters.all_contours = Clusters.all_contours.reshape((-1, 2))
-        centroids = Clusters.init_centroids(5)
-        centroids, indices = Clusters.run_kmeans(Clusters.all_contours, centroids)
+    # if Clusters.start_kmeans:
+    #     Clusters.all_contours = Clusters.all_contours.reshape((-1, 2))
+    #     centroids = Clusters.init_centroids(5)
+    #     centroids, indices = Clusters.run_kmeans(Clusters.all_contours, centroids)
 
-        for centroid in centroids:
-            centroid = tuple(np.uint32(centroid))
-            cv2.circle(frame, centroid, 20, (0, 255, 0), 3)
+    #     for centroid in centroids:
+    #         centroid = tuple(np.uint32(centroid))
+    #         cv2.circle(frame, centroid, 20, (0, 255, 0), 3)
 
     cv2.imshow("img", frame)
 
@@ -129,8 +128,10 @@ def real_server():
 
         try:
             connection, address = s.accept()
-            for frame in service_connection():
-                bound_frame(frame)
+            count = 0
+            for frame in service_connection(connection):
+                save_fake_frame(frame, count)
+                count += 1
 
         except Exception as e:
             print(f"Connection Failed ... {e}")
@@ -140,10 +141,11 @@ def test_server():
     frames_consumed = 0
     frame_w, frame_h = None, None
     while True:
-        for frame in fake_connection():
+        for file, frame in fake_connection():
             
-            if frame_w is None and frame_h is None:
-                frame_h, frame_w, _ = frame.shape
+            print(file)
+            time.sleep(1)
+            bound_frame(frame)
 
             frames_consumed += 1
 
@@ -156,3 +158,4 @@ if __name__ == "__main__":
     cv2.createTrackbar("MaxArea", "Parameters", 2277, 5000, empty)
     cv2.createTrackbar("MaxApprox", "Parameters", 0, 300, empty)
     test_server()
+    # real_server()
